2baf42a6917011bdc8dc6454f20ef413b6f28909
Diogo Cunha <diogofncunha@gmail.com>
2019-07-26 10:42:43 +0100

    feat: 🎸 added article payload type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export default function hello() {
-  console.log('hello');
+export interface ArticlePayload {
+  /**
+   * The title of an article
+   */
+  title?: string;
+  /**
+   * Description of the article.
+   */
+  description?: string;
+  /**
+   * The Markdown body, with or without a front matter.
+   */
+  body_markdown: string;
+  /**
+   * True if the article should be published right away, defaults to false.
+   */
+  published?: boolean;
+  /**
+   * A list of tags for the article.
+   */
+  tags?: string[];
+  /**
+   * The name of the series the article should be published within.
+   */
+  series?: string;
+  /**
+   * Your organization's ID, if you wish to create an article under an organization
+   */
+  organization_id?: string;
+  /**
+   * URL of the image to use as the cover
+   */
+  main_image?: string;
+  /**
+   * Canonical URL of the article (string, optional)
+   */
+  canonical_url?: string;
 }
