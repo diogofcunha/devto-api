72bb884bffb5eb0d33c35cb97886b9d0b2727899
Diogo Cunha <diogofncunha@gmail.com>
2019-07-26 11:41:25 +0100

    feat: 🎸 added create and update post functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import fetch from 'node-fetch';
+
+const DEV_TO_API_ORIGIN = `https://dev.to/api`;
+const ARTICLE_ROUTE = '/articles';
+
 export interface ArticlePayload {
   /**
    * The title of an article
    */
-  title?: string;
+  title: string;
   /**
    * Description of the article.
    */
@@ -35,4 +40,73 @@ export interface ArticlePayload {
    * Canonical URL of the article (string, optional)
    */
   canonical_url?: string;
+}
+
+export interface User {
+  name: string;
+  username: string;
+  twitter_username: null | string;
+  github_username: null | string;
+  website_url: null | string;
+  profile_image: string | null;
+  profile_image_90: string | null;
+}
+
+export interface ArticleResult {
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  cover_image: null | string;
+  readable_publish_date: string;
+  social_image: string;
+  tag_list: string;
+  tags: string[];
+  slug: string;
+  path: string;
+  url: string;
+  canonical_url: string;
+  comments_count: number;
+  positive_reactions_count: number;
+  created_at: Date;
+  edited_at: null | Date;
+  crossposted_at: null | Date;
+  published_at: null | Date;
+  last_comment_at: Date;
+  body_html: string;
+  body_markdown: string;
+  ltag_style: string[];
+  ltag_script: string[];
+  user: User;
+}
+
+async function articleMutation(articlePayload: ArticlePayload, apiKey: string, articleId?: number) {
+  let baseUrl = `${DEV_TO_API_ORIGIN}${ARTICLE_ROUTE}`;
+
+  if (articleId !== undefined) {
+    baseUrl = baseUrl.concat(`/${articleId}`);
+  }
+
+  const response = await fetch(`${DEV_TO_API_ORIGIN}${ARTICLE_ROUTE}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'api-key': apiKey,
+    },
+    body: JSON.stringify({article: articlePayload}),
+  });
+
+  if (!response.ok) {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+
+  return response.json();
+}
+
+export async function createArticle(articlePayload: ArticlePayload, apiKey: string) {
+  return await articleMutation(articlePayload, apiKey);
+}
+
+export async function updateArticle(articlePayload: ArticlePayload, apiKey: string, articleId: number) {
+  return await articleMutation(articlePayload, apiKey, articleId);
 }
